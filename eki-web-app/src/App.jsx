3a22063d042c81a6f15b7171f23d3a956b973945
Becky-234 <kiraborebecca04@gmail.com>
2026-03-10 12:48:27 +0300

    Fix link on navbar

---
+++ eki-web-app/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn'; 
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+import AdminSignin from './pages/AdminSignin';
+import AccountSettingsPage from './pages/AccountSetting';
+import VendorOnboarding from "./pages/VendorOnboarding";
+import Settings from './pages/Settings';
+import AdminDashboard from './pages/AdminDashboard';
+import VendorDashboard from './pages/VendorDashboard';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/admin-signin" element={<AdminSignin />} />
+        <Route path="/account-settings" element={<AccountSettingsPage />} />
+         <Route path="/VendorOnboarding" element={<VendorOnboarding />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route path="/vendordashboard" element={<VendorDashboard />} />
+         <Route path="*" element={<div className="p-10 text-center text-red-500">404 - Page Not Found</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
