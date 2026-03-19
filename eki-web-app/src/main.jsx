2637489f3fa17775a054ebde7810df7bd4cbd0ed
Becky-234 <kiraborebecca04@gmail.com>
2026-03-19 16:04:11 +0300

    Import i18n

--- eki-web-app/src/main.jsx
+++ eki-web-app/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
+import './i18n' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
