e419fffc0bc20c0d1210012106d617affe4182b4
AvukiBlessing-hub <avukiblessingisaac@gmail.com>
2026-03-24 19:12:59 +0300

    add modal on the product dashboard page

--- eki-web-app/src/pages/Login.jsx
+++ eki-web-app/src/pages/Login.jsx
@@ -218,7 +218,7 @@ const Login = () => {
                 disabled={isLoading || !isFormFilled}
                 className={`w-full h-12 rounded-xl font-bold transition-all duration-300
                   ${isFormFilled && !isLoading
-                    ? 'bg-[#efb034] hover:bg-[#d99c1c] hover:-translate-y-1 hover:shadow-lg text-white cursor-pointer' 
+                    ? 'bg-[#efb034] hover:bg-[#d99c1c] btn-roundhover:-translate-y-1 hover:shadow-lg text-white cursor-pointer' 
                     : 'bg-gray-300 cursor-not-allowed text-white/70'
                   }`}
               >
